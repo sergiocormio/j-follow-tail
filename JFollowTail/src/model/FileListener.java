1223ae3bbafc71574976ec703de86ca768fa8523
sergiocormio <sergiocormio@ea4bfc6c-ec47-656b-6a42-474bfa1c8ce0>
2014-05-03 20:09:07 +0000

    Now depends on JAVA 6

--- JFollowTail/src/model/FileListener.java
+++ JFollowTail/src/model/FileListener.java
@@ -1,19 +1,21 @@
 package model;
 
-import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
-import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.WatchEvent;
-import java.nio.file.WatchEvent.Kind;
-import java.nio.file.WatchKey;
-import java.nio.file.WatchService;
+
+//These imports works in JAVA 7
+//import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+//import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+//import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.nio.file.WatchEvent;
+//import java.nio.file.WatchEvent.Kind;
+//import java.nio.file.WatchKey;
+//import java.nio.file.WatchService;
 
 /**
  * Class in charge of listening file changes from file system
@@ -24,7 +26,7 @@ import java.nio.file.WatchService;
 public class FileListener implements Runnable {
 	public static final String FILE_WAS_MODIFIED = "File was modified";
 	private boolean stop = false;
-	private WatchService service;
+//	private WatchService service; //Only for JAVA7
 	private File file;
 	private PropertyChangeSupport propertyChangeSupport;
 	private long lastLength = 0l;
@@ -38,15 +40,16 @@ public class FileListener implements Runnable {
 		new Thread(this).start();
 	}
 
-	@SuppressWarnings("unused")
-	private void setWatcherService() throws IOException {
-		// Path must be a directory
-		Path path = Paths.get(file.getParentFile().toURI());
-		service = path.getFileSystem().newWatchService();
-		// We register the path to the service
-		// We watch for creation events
-		path.register(service, ENTRY_CREATE, ENTRY_MODIFY);
-	}
+//Only for JAVA7
+//	@SuppressWarnings("unused")
+//	private void setWatcherService() throws IOException {
+//		// Path must be a directory
+//		Path path = Paths.get(file.getParentFile().toURI());
+//		service = path.getFileSystem().newWatchService();
+//		// We register the path to the service
+//		// We watch for creation events
+//		path.register(service, ENTRY_CREATE, ENTRY_MODIFY);
+//	}
 
 	public void addPropertyChangeListener(PropertyChangeListener listener) {
 		propertyChangeSupport.addPropertyChangeListener(listener);
@@ -74,46 +77,47 @@ public class FileListener implements Runnable {
 		}
 	}
 
-	@SuppressWarnings("unused")
-	private void doRunWithWatcherService() {
-		WatchKey key = null;
-
-		while (!stop) {
-			try {
-				key = service.take();
-
-				// Dequeueing events
-				Kind<?> kind = null;
-				for (WatchEvent<?> watchEvent : key.pollEvents()) {
-					// Get the type of the event
-					kind = watchEvent.kind();
-					if (OVERFLOW == kind) {
-						continue; // loop
-					} else if (ENTRY_CREATE == kind) {
-						// A new Path was created
-						// Path newPath = ((WatchEvent<Path>)
-						// watchEvent).context();
-						// Output
-						// System.out.println("New path created: " + newPath);
-					} else if (ENTRY_MODIFY == kind) {
-						// A Modification happened
-						Path modifiedPath = ((WatchEvent<Path>) watchEvent)
-								.context();
-						if (modifiedPath.endsWith(file.getName())) {
-							// Output
-							propertyChangeSupport.firePropertyChange(FILE_WAS_MODIFIED, null, file);
-						}
-					}
-				}
-
-				if (!key.reset()) {
-					break; // loop
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+	//Only for JAVA7
+//	@SuppressWarnings("unused")
+//	private void doRunWithWatcherService() {
+//		WatchKey key = null;
+//
+//		while (!stop) {
+//			try {
+//				key = service.take();
+//
+//				// Dequeueing events
+//				Kind<?> kind = null;
+//				for (WatchEvent<?> watchEvent : key.pollEvents()) {
+//					// Get the type of the event
+//					kind = watchEvent.kind();
+//					if (OVERFLOW == kind) {
+//						continue; // loop
+//					} else if (ENTRY_CREATE == kind) {
+//						// A new Path was created
+//						// Path newPath = ((WatchEvent<Path>)
+//						// watchEvent).context();
+//						// Output
+//						// System.out.println("New path created: " + newPath);
+//					} else if (ENTRY_MODIFY == kind) {
+//						// A Modification happened
+//						Path modifiedPath = ((WatchEvent<Path>) watchEvent)
+//								.context();
+//						if (modifiedPath.endsWith(file.getName())) {
+//							// Output
+//							propertyChangeSupport.firePropertyChange(FILE_WAS_MODIFIED, null, file);
+//						}
+//					}
+//				}
+//
+//				if (!key.reset()) {
+//					break; // loop
+//				}
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
+//	}
 
 	public synchronized void stop() {
 		stop = true;
