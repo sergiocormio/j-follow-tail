b3e7ba921d5ca3b2215e26e8a5caa43dd36de75e
sergiocormio <sergiocormio@ea4bfc6c-ec47-656b-6a42-474bfa1c8ce0>
2014-05-03 20:03:07 +0000

    Foreground color feature first implementation.

--- JFollowTail/src/view/highlightings/Highlighting.java
+++ JFollowTail/src/view/highlightings/Highlighting.java
@@ -5,10 +5,12 @@ import java.awt.Color;
 public class Highlighting {
 	private String token;
 	private Color backgroundColor;
+	private Color foregroundColor;
 	
-	public Highlighting(String token, Color backgroundColor) {
+	public Highlighting(String token, Color backgroundColor, Color foregroundColor) {
 		this.token = token;
 		this.backgroundColor = backgroundColor;
+		this.foregroundColor = foregroundColor;
 	}
 	
 	public Color getBackgroundColor() {
@@ -16,6 +18,13 @@ public class Highlighting {
 	}
 	public void setBackgroundColor(Color backgroundColor) {
 		this.backgroundColor = backgroundColor;
+	}
+	public Color getForegroundColor() {
+		return foregroundColor;
+	}
+	
+	public void setForegroundColor(Color foregroundColor) {
+		this.foregroundColor = foregroundColor;
 	}
 	public String getToken() {
 		return token;
@@ -29,4 +38,5 @@ public class Highlighting {
 		Highlighting otherObj = (Highlighting) obj;
 		return token.equals(otherObj.getToken()) && backgroundColor.equals(otherObj.getBackgroundColor());
 	}
+
 }
